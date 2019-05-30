7aecbcc93dc791f40830646d87476db0a59ed382
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-30 10:18:36 +0800

    Add files via upload

---
+++ Script-028d-SM-Partial Regression.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import statsmodels.api as sm
+from patsy import dmatrices
+import os
+os.chdir('/Users/pauline/Documents/Python')
+df = pd.read_csv("Tab-Morph.csv")
+df = df.dropna()
+sns.set_style('darkgrid')
+
+sm.graphics.plot_partregress('sedim_thick', 'slope_angle', ['plate_carol', 'profile'], data=df, 
+                             obs_labels=False)
+plt.title("Partial regression of slope angle vs sediment thickness: \n Caroline Plate, 25 bathymetric profiles")
+plt.annotate('D', xy=(-0.01, 1.06), xycoords="axes fraction", fontsize=18,
+           bbox=dict(boxstyle='round, pad=0.3', fc='w', edgecolor='grey', linewidth=1, alpha=0.9))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
